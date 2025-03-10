8064830e6026048a0c6cc193f44383e950cbebb3
Luqman Booso <131631632+luqmanbooso@users.noreply.github.com>
2025-03-10 17:03:49 +0530

    add routers

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,23 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import BidForm from './components/BidForm'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
+import NoPage from './Pages/NoPage'
+import ProjectDetails from './Pages/ProjectDetails'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <BidForm />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/bid-form' element={<BidForm/>} />
+        <Route path="/project-details" element={<ProjectDetails />} />
+        {/* Fallback route */}
+        <Route path="*" element={<NoPage />} />
+      </Routes>
     </>
   )
 }
