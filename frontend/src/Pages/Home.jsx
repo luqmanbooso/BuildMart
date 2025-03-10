08891a256ed369985ff3f0168b3d40a1c9f963b5
Luqman Booso <131631632+luqmanbooso@users.noreply.github.com>
2025-03-10 17:15:17 +0530

    home page

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>Home
+        <div>
+            <h1>Home</h1>
+            <p>Welcome to the Home Page</p>
+        </div>
+    </div>
   )
 }
 
