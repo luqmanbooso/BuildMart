26f1cb8006992e65ef8d3cff55588067c5e2fcef
Luqman Booso <131631632+luqmanbooso@users.noreply.github.com>
2025-03-11 11:26:01 +0530

    solved the server js warning

--- backend/server.js
+++ backend/server.js
@@ -10,14 +10,9 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-mongoose.connection.once('open', () => {
-  console.log('Connected to MongoDB');
-});
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 // Routes
 app.get('/', (req, res) => {
