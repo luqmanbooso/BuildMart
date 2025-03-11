6dfb0ab857c51512eebb6f9a91619b8d2361491e
Luqman Booso <131631632+luqmanbooso@users.noreply.github.com>
2025-03-11 10:46:45 +0530

    auctionspage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import NoPage from './Pages/NoPage'
 import ProjectDetails from './Pages/ProjectDetails'
 import Login from './Pages/login'
 import Shop from './Pages/shop'
+import AuctionsPage from './Pages/AuctionsPage'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -21,6 +22,7 @@ function App() {
         <Route path="/project-details" element={<ProjectDetails />} />
         <Route path="/login" element={<Login />} />
         <Route path="/Shop" element={<Shop />} />
+        <Route path="/auctions" element={<AuctionsPage />} />
         {/* Fallback route */}
         <Route path="*" element={<NoPage />} />
         
